051a36dd7bbd75a7e427953e00369dd36d14ee4d
Paul Bryan Roberts <NewForester@users.noreply.github.com>
2017-11-05 09:28:40 +0000

    Add exercism exercies #9, iteration #3 - Isogram

--- exercism/Isogram.hs
+++ exercism/Isogram.hs
@@ -5,15 +5,14 @@ import Data.Char (toLower, isLetter)
 
 isIsogram :: String -> Bool
 isIsogram string =
-    length (clean string) == size (fromList (clean string))
-    where clean string = [toLower c | c <- string, isLetter c]
+    checkIsogram [toLower c | c <- string, isLetter c]
+    where checkIsogram string
+            | length' <= 26 = size (fromList string) == length'
+            | otherwise     = False
+            where length' = length string
 
 -- ^
--- A two line solution using libraries after a suggestion by @teehemkay.
--- It uses set rather than nub and so has a better O.
--- For isogram, with N bounded at 26, the O is not important
---
--- However, it is good to know that using set is not onerous.
---
--- Note that to import Data.Set I had to change the build parameters.
+-- Final word.
+-- This iteration incorporate the upper bound:
+-- no string longer than 26 characters can possibly be an isogram.
 --

df0938a02de2842966a8546884cdd83ddad067f8
Paul Bryan Roberts <NewForester@users.noreply.github.com>
2017-11-07 10:53:42 +0000

    Add exercism exercise #10, iteration #2 - Perfect Numbers

--- exercism/PerfectNumbers.hs
+++ exercism/PerfectNumbers.hs
@@ -5,27 +5,18 @@ data Classification = Deficient | Perfect | Abundant deriving (Eq, Show)
 classify :: Int -> Maybe Classification
 classify nn
     | nn < 1    = Nothing
-    | nn == 1   = Just Deficient
-    | otherwise = Just $ dandy nn $ aliquot_sum nn
+    | otherwise = case compare nn aliquot_sum of
+        LT -> Just Abundant
+        GT -> Just Deficient
+        EQ -> Just Perfect
     where
-        dandy nn nn'
-            | nn < nn'  = Abundant
-            | nn > nn'  = Deficient
-            | otherwise = Perfect
-        bound = floor $ sqrt $ fromIntegral $ nn
-        aliquot_sum nn =
+        aliquot_sum =
             sum [x + div nn x | x <- [2 .. bound], mod nn x == 0] + correction
-            where
-                correction
-                    | bound * bound == nn   = 1 - bound
-                    | otherwise             = 1
+        bound = floor $ sqrt $ fromIntegral $ nn
+        correction
+            | bound * bound == nn   = 1 - bound
+            | otherwise             = 1
 
 --
--- It looks as if there are only 10 other solutions worldwide.
--- Only one other used sqrt but one used some really advanced library function.
---
--- Two used compare, which I like so much I will rewrite this.
---
--- I have used nn in correction without passing it down.
--- For consistencies sake, I should not pass nn down to dandy and aliquot sum.
+-- Much neater, liked by teehemkay
 --

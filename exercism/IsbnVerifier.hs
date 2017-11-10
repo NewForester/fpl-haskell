9c2118d31fd4d797e354dd561d7f4cc864dfb1fa
Paul Bryan Roberts <NewForester@users.noreply.github.com>
2017-11-10 20:33:51 +0000

    Add exercism exercise #11, iteration #3 - IsbnVerifier

--- exercism/IsbnVerifier.hs
+++ exercism/IsbnVerifier.hs
@@ -5,16 +5,16 @@ import Data.Char (digitToInt)
 
 isbn :: String -> Bool
 isbn string
-    | string =~ sinDashes :: Bool   = validate $ convert string
-    | string =~ conDashes :: Bool   = validate $ convert $ filter (/= '-') string
-    | otherwise                     = False
+    | length string == 10 && string =~ sinDashes :: Bool = validate $ convert string
+    | length string == 13 && string =~ conDashes :: Bool = validate $ convert $ filter (/= '-') string
+    | otherwise = False
     where
-        sinDashes = "^[0-9]{9}[0-9X]{1}$"
-        conDashes = "^[0-9]{1}-[0-9]{3}-[0-9]{5}-[0-9X]{1}$"
+        sinDashes = "^[0-9]{9}[0-9X]$"
+        conDashes = "^[0-9]+-[0-9]+-[0-9]+-[0-9X]$"
 
-        validate string = sum (zipWith (*) string [10,9..1]) `mod` 11 == 0
+        validate numbers = sum (zipWith (*) numbers [1..10]) `mod` 11 == 0
         convert string = [if c == 'X' then 10 else digitToInt c | c <- string]
 
 --
--- Yes, zipwith was what I was looking for.
+-- The dashes are significant but their positions are not as fixed as I had thought.
 --

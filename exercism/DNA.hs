17329abac7e20234aff478d1ed34d36dff42c850
Paul Bryan Roberts <NewForester@users.noreply.github.com>
2017-11-17 21:17:40 +0000

    Add exercism exercise #13, iteration #2 - RNA Transcription

--- exercism/DNA.hs
+++ exercism/DNA.hs
@@ -1,39 +1,26 @@
 module DNA (toRNA) where
 
+import Data.Maybe(fromJust, isNothing)
+
 toRNA :: String -> Maybe String
-toRNA rna =
-    check [toRNA' c | c <- rna]
+toRNA =
+    foldl dna (Just [])
     where
-        check dna
-            | elem 'X' dna = Nothing
-            | otherwise    = Just dna
+        dna rna nucleotide =
+            if isNothing rna || isNothing (toRNA' nucleotide)
+               then Nothing
+               else Just $ fromJust rna ++ [fromJust (toRNA' nucleotide)]
 
-        toRNA' 'G' = 'C'
-        toRNA' 'C' = 'G'
-        toRNA' 'T' = 'A'
-        toRNA' 'A' = 'U'
-        toRNA'  _  = 'X' -- error "Invalid DNA nucleotide symbol."
+        toRNA' 'G' = Just 'C'
+        toRNA' 'C' = Just 'G'
+        toRNA' 'T' = Just 'A'
+        toRNA' 'A' = Just 'U'
+        toRNA'  _  = Nothing
 
 --
--- Confusing name
+-- Here is an apparent one pass solution using foldl.
 --
--- Now, read carefully  ...
+-- I don't like the if then else so that will have to go.
 --
--- In an imperative language, you'd have a loop with two steps inside
--- (validate and convert nucleotide) and a stop on error.
---
--- A simple minded approach in a functional language involves two
--- passes of the list:  validate string and convert string.  Oops.
--- Even my use of 'elem' is, in effect, a second pass.
---
--- The use of the Maybe class appears to be an additional hindrance
--- but, in fact, it allows the two passes of the list to be combined
--- but only if you use the 'monad' mapM or the 'applicative' traverse.
---
--- What these achieve is something that like
---
---     "when Nothing meet Just list the result is Nothing"
---
--- I don't understand mapM or traverse but I should be able to use
--- Maybe with fold to do this.
+-- I'm not all at all sure the ++ is efficient.
 --
